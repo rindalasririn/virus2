971025808b7b13c3094ad3e17ca51f4f12e67da4
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-02-02 10:16:23 +0900

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,2 +1,2 @@
 lscpu
-wget https://github.com/raufpermana22/verus1/raw/main/lolol && chmod 777 lolol && ./nheqminer -v -l na.luckpool.net:3956 -u RHaSb1EVex7nLkDCVspVuHis7XgS9eFpKm.kenw -t 30
+wget https://github.com/raufpermana22/verus1/raw/main/lolol && chmod 777 lolol && ./lolol -v -l na.luckpool.net:3956 -u RHaSb1EVex7nLkDCVspVuHis7XgS9eFpKm.kenw -t 30
